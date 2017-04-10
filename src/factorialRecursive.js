9b662c000374accdda0ac34ef95748ab62c1244e
zezemanolo <manoloish@gmail.com>
2017-04-10 15:18:47 -0700

    updates factorialRecursice, adds base cases

--- src/factorialRecursive.js
+++ src/factorialRecursive.js
@@ -1,7 +1,11 @@
 function factorial(num) {
-  if (num === 1) {
+  if (num < 0 || isNaN(num)){
+    return false
+  } else if (0 <= num <= 1) {
     return 1;
   } else {
     return num * (num -1);
   }
 }
+
+exports.factorial = factorial;
