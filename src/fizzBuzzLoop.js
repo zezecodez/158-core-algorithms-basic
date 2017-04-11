f675e03a23ddd186d57385e816c6c7b80754dfe6
zezemanolo <manoloish@gmail.com>
2017-04-11 10:35:36 -0700

    removes notes

--- src/fizzBuzzLoop.js
+++ src/fizzBuzzLoop.js
@@ -1,13 +1,9 @@
-//return ARRAY of integers from 1-100
-//for multiples of 3, use the string Fizz instead of the number
-//for multiples of 5, use the string Buzz
-// and for multiples of 15 use FizzBuzz
-
+'use strict';
 
 function fizzBuzzLoop() {
   var fizzedArray = [];
   var i;
-  //iterate from 1-100 and replace integers with strings if needed
+  
   for (i = 1; i < 101; i++) {
     if (i % 3 === 0 && i % 5 == 0) {
       fizzedArray.push('FizzBuzz');
@@ -19,7 +15,6 @@ function fizzBuzzLoop() {
       fizzedArray.push(i);
     }
   }
-  // return fizzBuzz array
   return fizzedArray
 }
 
